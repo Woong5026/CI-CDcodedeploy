f6ba5b7ed749af53aef427320c9c4c4d44e604ae
postwoong <ktw5026@naver.com>
2023-03-17 02:02:24 +0900

    Initial commit

---
+++ src/main/java/com/example/cemtoo2/Cemtoo2Application.java
@@ -0,0 +1,13 @@
+package com.example.cemtoo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cemtoo2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cemtoo2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cemtoo2/Cemtoo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cemtoo2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cemtoo2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
